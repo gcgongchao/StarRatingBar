8e46e415e6d2b7d6fd8e6ac7d71f97cd44363bf0
gcgongchao <gc_gongchao@163.com>
2015-07-28 11:29:34 +0800

    add the custom attributes and read the attributes

--- src/com/gc/starratingbar/views/LStarRatingBar.java
+++ src/com/gc/starratingbar/views/LStarRatingBar.java
@@ -1,6 +1,12 @@
 package com.gc.starratingbar.views;
 
+import com.gc.starratingbar.R;
+
+import android.R.integer;
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.widget.LinearLayout;
 /**
@@ -9,9 +15,30 @@ import android.widget.LinearLayout;
  * @function 水平或垂直的自定义RatingBar
  * @time 2015/07/27
  */
+@SuppressLint("Recycle") 
 public class LStarRatingBar extends LinearLayout{
 
 	private Context mContext;
+	/**
+	 * 值为0时水平排列，值为1时垂直排列
+	 */
+	private int orientation;
+	/**
+	 * 根据值的不同应用不同的动画效果
+	 */
+	private int animationEffect;
+	/**
+	 * 设置未选中的图片资源
+	 */
+	private Drawable starUnselect;
+	/**
+	 * 设置选中的图片资源
+	 */
+	private Drawable starSelected;
+	/**
+	 * 设置选中的星星数量
+	 */
+	private int starSelectedNum;
 	public LStarRatingBar(Context context) {
 		this(context, null);
 		
@@ -20,7 +47,15 @@ public class LStarRatingBar extends LinearLayout{
 	public LStarRatingBar(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		this.mContext=context;
-		
+		TypedArray mTypedArray=context.obtainStyledAttributes(attrs, R.styleable.LStarRatingBar);
+		//读取自定义属性的值
+		orientation=mTypedArray.getInt(R.styleable.LStarRatingBar_orientation, 0);
+		animationEffect=mTypedArray.getInt(R.styleable.LStarRatingBar_animationEffect, 0);
+		starUnselect=mTypedArray.getDrawable(R.styleable.LStarRatingBar_starUnselect);
+		starSelected=mTypedArray.getDrawable(R.styleable.LStarRatingBar_starSelected);
+		starSelectedNum=mTypedArray.getInt(R.styleable.LStarRatingBar_starSelectedNum, 0);
 	}
+	
+	
 
 }
